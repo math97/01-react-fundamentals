4714d1765b834c97587f66dc8c5e437ba3c389f0
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-11-10 22:42:48 +0000

    improving form with validation and css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -41,7 +41,12 @@ export function Post({author,publishedAt,content}){
   }
 
   function handleNewCommentChange(){
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid(){
+    event.target.setCustomValidity("Esse campo é obrigatório CARALHO")
   }
 
   function deleteComment(commentToDelete){
@@ -50,6 +55,8 @@ export function Post({author,publishedAt,content}){
     })
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -84,10 +91,14 @@ export function Post({author,publishedAt,content}){
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
       <div className={styles.commentList}>
